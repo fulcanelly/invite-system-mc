f8a358533b9b187e713bfd42bd379b1f0a49f3b2
fulcanelly <dallasxfulcanelly@gmail.com>
2021-07-27 18:23:07 +0300

    add ability to disable logger in guard

--- guard/src/main/java/me/fulcanelly/inguard/logger/TargetLogger.java
+++ guard/src/main/java/me/fulcanelly/inguard/logger/TargetLogger.java
@@ -2,6 +2,9 @@ package me.fulcanelly.inguard.logger;
 
 import java.io.PrintStream;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -11,12 +14,18 @@ public abstract class TargetLogger {
     PrintStream out = System.out;
     final String self;
 
+    @Inject @Named("disable logger")
+    boolean disable = false;
+
     TargetLogger(String self) {
         this.self = self;
         println("created");
     }
     
     void println(String line) {
+        if (disable) {
+            return;
+        }
         var current = Thread.currentThread();
         var wasName = current.getName();
         current.setName(self);
