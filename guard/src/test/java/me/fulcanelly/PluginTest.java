9f38ba9f50b08a66e4ca910c4b65bd18756d1232
fulcanelly <dallasxfulcanelly@gmail.com>
2021-07-27 21:18:53 +0300

    remove needless

--- guard/src/test/java/me/fulcanelly/PluginTest.java
+++ guard/src/test/java/me/fulcanelly/PluginTest.java
@@ -1,18 +1,18 @@
 package me.fulcanelly;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 import java.net.ServerSocket;
 
 import com.google.inject.Guice;
 
-import org.bukkit.Bukkit;
-import org.junit.jupiter.api.Disabled;
+import org.bukkit.plugin.Plugin;
 import org.junit.jupiter.api.Test;
 
 import lombok.SneakyThrows;
 import me.fulcanelly.inguard.MainModule;
 import me.fulcanelly.inguard.client.RequestPlanner;
-import me.fulcanelly.mocks.MockPlugin;
 
 public class PluginTest {
     
@@ -21,17 +21,6 @@ public class PluginTest {
         var socket = new ServerSocket(6997);
         socket.accept();
         socket.close();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Bukkit.getServer());
-        new Thread(PluginTest::mockServer).start();
-        var mainModule = new MainModule(
-            new MockPlugin()
-        );
-        Guice.createInjector(mainModule)
-            .getInstance(RequestPlanner.class)
-            .start();
     }
 
     @Test
